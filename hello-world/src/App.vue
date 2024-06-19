c2bb461ef5c05218576731e0446326dc2810fdf6
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-19 10:26:09 +0530

    binding classes

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,27 +1,50 @@
 <template>
-  <div>{{greet}} {{name}}</div> <!--binding data-->
-  <div v-text="channel"></div> <!--Directive-->
+  <div>{{ greet }} {{ name }}</div>
+  <!--binding data-->
+
   <!--  <div v-text="name">Hello</div> v textwill override element children-->
-
-  
-
+  <div v-html="channel"></div>
+  <!--you only the content that only trust-->
+  <div v-html="hack"></div>
+  <h2 v-bind:id="headingID">Heading</h2>
+  <button v-bind:disabled="isDiabled">Bind</button>
+  <h2 class="underline">Underlined Text</h2>
+  <h2 class="underline" v-bind:class="status">Danger</h2>
+  <h2 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h2>
+  <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">Soldout movie</h2>
+  <h2 v-bind:class="['new', 'promoted']">Newly Promoted Movie</h2>
+  <h2 v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">
+    Array Conditional Movie
+  </h2>
+  <h2
+    v-bind:class="{
+      promoted: isPromoted,
+      new: !isSoldOut,
+      'sold-out': isSoldOut,
+    }"
+  >
+    Object conditional movie
+  </h2>
 </template>
 <!--recommended way is mustash way-->
 
-
 <script>
-
 export default {
-  name: 'App',
-  data(){
-    return{
-      greet:'Byee',
-      name:'Vihanga',
-      channel:'Chanux Bro'
-    }
-  }
-  
-}
+  name: "App",
+  data() {
+    return {
+      greet: "Byee",
+      name: "Vihanga",
+      channel: "<b>Chanux Bro<b>",
+      hack: `<a href="#" onclick="alert('You have been hacked!')" > Win a price </a>`,
+      headingID: "heading",
+      isDiabled: true,
+      status: "danger",
+      isPromoted: true,
+      isSoldOut: true,
+    };
+  },
+};
 </script>
 
 <style>
@@ -32,5 +55,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.underline {
+  text-decoration: underline;
+}
+
+.promoted {
+  font-style: italic;
+}
+
+.new {
+  color: olivedrab;
+}
+
+.sold-out {
+  color: red;
 }
 </style>
