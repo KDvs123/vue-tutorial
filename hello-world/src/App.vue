8a27557e9d77201e842d6f4623aa1e925ff4b2ba
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-19 16:46:54 +0530

    Form handling-Part1

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -3,9 +3,9 @@
   <!--binding data-->
 
   <!--  <div v-text="name">Hello</div> v textwill override element children-->
-  <div v-html="channel"></div>
+  <!-- <div v-html="channel"></div> -->
   <!--you only the content that only trust-->
-  <div v-html="hack"></div>
+  <!-- <div v-html="hack"></div>
   <h2 :id="headingID">Heading</h2>
   <button :disabled="isDiabled">Bind</button>
   <h2 class="underline">Underlined Text</h2>
@@ -33,32 +33,35 @@
     padding:'20px'
   }">Font Size</h2>
   <h2 v-bind:style="headerStyleObject">Font Size</h2>
-  <div v-bind:style="[baseStyleObject,succssStyleObject]">Sucess Style</div><!--styles written in the last style object overrides previous style object -->
-  <div v-bind:style="[baseStyleObject,dangerStyleObjec]">Danger Style</div>
-  
+  <div v-bind:style="[baseStyleObject,succssStyleObject]">Sucess Style</div> -->
+  <!--styles written in the last style object overrides previous style object -->
+  <!-- <div v-bind:style="[baseStyleObject,dangerStyleObjec]">Danger Style</div>
+   -->
   <!--Conditional Rendering-->
-  <h2 v-if="num===0">The number is zero</h2>
+  <!-- <h2 v-if="num===0">The number is zero</h2>
   <h2 v-else-if="num<0" >The number is negative</h2>
   <h2 v-else-if="num>0">The number is positive</h2>
   <h2 v-else>Its not a number</h2>
-
+ -->
 
 
   <!--actes as an invisible div-->
-  <template v-if="display">
+  <!-- <template v-if="display">
     <h2>Viswas</h2>
     <h2>Chanux Bro</h2>
     <h2>Vue</h2>
 
-  </template>
+  </template> -->
 
   
-  <h2 v-show="showElement">Using v-show</h2><!--when show element is false it just hidden if you have an element that's gonna be toggleing this is the effective way-->
-  <h2 v-if="showElement">Using v-if</h2><!--when show element is false it just not present at all-->
+  <!-- <h2 v-show="showElement">Using v-show</h2> -->
+  <!--when show element is false it just hidden if you have an element that's gonna be toggleing this is the effective way-->
+  <!-- <h2 v-if="showElement">Using v-if</h2> -->
+  <!--when show element is false it just not present at all-->
 
   <!--list rendering using v-for directive-->
 
-  <h2 v-for="name in names" :key="name">{{name}}</h2>
+  <!-- <h2 v-for="name in names" :key="name">{{name}}</h2>
   <h2 v-for="(name , index) in names" :key="index">{{index}} {{name}}</h2>
   <h2 v-for="(name , index) in fullNames" :key="index">{{name.first}} {{name.last}}</h2>
   <div v-for="actor in actors" :key="actor.name">
@@ -66,7 +69,7 @@
     <h3 v-for="movie in actor.movies" :key="movie">{{movie}}</h3>
 
 
-  </div>
+  </div> -->
 
   <!--object-->
 
@@ -90,21 +93,68 @@
    </template> -->
 
    
-    <h2>addMethod {{add(2,3,4)}}</h2>
+    <!-- <h2>addMethod {{add(2,3,4)}}</h2>
     <h2>addMethod {{add(10,15,20)}}</h2>
     <h2>Multiply method {{multiply(10)}}</h2>
     <h2>{{name1}}</h2>
     <div>
          <button @mouseover="changeName($event) , increment(1, $event)">Change Name</button>
-    </div>
+    </div> -->
 
-    <h2>{{count}}</h2>
+    <!-- <h2>{{count}}</h2>
     <div>
       <button @click="increment(2, $event)">Increment Button</button>
       <button v-on:click="decrement(2)">Decrement Button</button>
        <button v-on:click="increment(5)">Increment Button</button>
       <button v-on:click="decrement(5)">Decrement Button</button>
+    </div> -->
+
+
+    <!--Form Handling-->
+    <div>
+      <pre>{{JSON.stringify(formValues,null ,2)}}</pre>
     </div>
+
+    <form action="
+    ">
+
+    <div>
+      <label for="name">Name:</label>
+      <input type="text" id="name" v-model="formValues.name">
+    </div>
+
+    <div>
+      <label for="profile">Profile Summary</label>
+        <textarea  id="profile" cols="30" rows="10" v-model="formValues.profileSummary"></textarea>
+
+      </div>
+    <div>
+      <label for="country">Country</label>
+      <select name="" id="country" v-model="formValues.country">
+        <option value="">Select a country</option>
+        <option value="india">India</option>
+        <option value="vietnam">Vietnam</option>
+        <option value="Srilanka">Srilanka</option>
+      </select>
+    </div>
+
+    <div>
+      <label for="job-location">Job Location</label>
+      <select name="" id="job-location" multiple v-model="formValues.jobLocation">
+        <option value="india">India</option>
+        <option value="vietnam">Vietnam</option>
+        <option value="Srilanka">Srilanka</option>
+      </select>
+    </div>
+
+
+    </form>
+      
+   
+
+
+
+
    
 </template>
 <!--recommended way is mustash way-->
@@ -115,60 +165,66 @@ export default {
   
   data() {
     return {
-      count:0,
-      name1:'Wishwas',
-      greet: "Byee",
-      name: "Vihanga",
-      channel: "<b>Chanux Bro<b>",
-      hack: `<a href="#" onclick="alert('You have been hacked!')" > Win a price </a>`,
-      headingID: "heading",
-      isDiabled: true,
-      status: "danger",
-      isPromoted: true,
-      isSoldOut: true,
-      heightlightColor:'orange',
-      headerSize:50,
-      headerStyleObject:{
-        fontSize:'50px',
-        padding:'20px',
-        color:'orange'
-      },
-      baseStyleObject:{
-        fontSize:'50px',
-        padding:'10px',
-      },
-      succssStyleObject:{
-        color:'green',
-        backgroundColor:'lightgreen',
-        border:'1px solid green',
-        padding:'20px',
+      formValues:{
+        name:'',
+        profileSummary:'',
+        country:'',
+        jobLocation:[]
+      }
+      // count:0,
+      // name1:'Wishwas',
+      // greet: "Byee",
+      // name: "Vihanga",
+      // channel: "<b>Chanux Bro<b>",
+      // hack: `<a href="#" onclick="alert('You have been hacked!')" > Win a price </a>`,
+      // headingID: "heading",
+      // isDiabled: true,
+      // status: "danger",
+      // isPromoted: true,
+      // isSoldOut: true,
+      // heightlightColor:'orange',
+      // headerSize:50,
+      // headerStyleObject:{
+      //   fontSize:'50px',
+      //   padding:'20px',
+      //   color:'orange'
+      // },
+      // baseStyleObject:{
+      //   fontSize:'50px',
+      //   padding:'10px',
+      // },
+      // succssStyleObject:{
+      //   color:'green',
+      //   backgroundColor:'lightgreen',
+      //   border:'1px solid green',
+      //   padding:'20px',
 
-      },dangerStyleObjec:{
-        color:'dark-red',
-        backgroundColor:'red',
-        border:'1px solid darkred'
-      },
-      num:5,
-      display:true,
-      showElement:true,
-      names:['Bruce','Mark','diana'],
-      fullNames:[
-        {first:'Bruce', last:'Wayne'},
-        {first:'Clark', last:'Kent'},
-        {first:'Princess', last:'Diana'},
-      ],
-      actors:[
-        {name:'Christian Bale' , movies: ['Batman', 'The Prestige']},
-        {name: 'Di Caprio' , movies: ['Titanic' , 'Inception']}
-      ],
+      // },dangerStyleObjec:{
+      //   color:'dark-red',
+      //   backgroundColor:'red',
+      //   border:'1px solid darkred'
+      // },
+      // num:5,
+      // display:true,
+      // showElement:true,
+      // names:['Bruce','Mark','diana'],
+      // fullNames:[
+      //   {first:'Bruce', last:'Wayne'},
+      //   {first:'Clark', last:'Kent'},
+      //   {first:'Princess', last:'Diana'},
+      // ],
+      // actors:[
+      //   {name:'Christian Bale' , movies: ['Batman', 'The Prestige']},
+      //   {name: 'Di Caprio' , movies: ['Titanic' , 'Inception']}
+      // ],
 
-      myInfo:{
-        name:'Vihanga',
-        channel:'VSK',
-        course:'Vue 3'
-      },
+      // myInfo:{
+      //   name:'Vihanga',
+      //   channel:'VSK',
+      //   course:'Vue 3'
+      // },
 
-      baseMultiplier:5
+      // baseMultiplier:5
 
     
       
@@ -178,25 +234,25 @@ export default {
   },
    methods:{
 
-    add(a,b,c){
-      return a+b+c
-    },
+    // add(a,b,c){
+    //   return a+b+c
+    // },
 
-    multiply(num){
-      return num*this.baseMultiplier
+    // multiply(num){
+    //   return num*this.baseMultiplier
 
-    }
+    // }
 
-    ,increment(num){
-      this.count+=num;
-    }
-    , decrement(num){
-      this.count-=num
-    },
-    changeName(event){
-      this.name1='batman'
-      console.log(event)
-    }
+    // ,increment(num){
+    //   this.count+=num;
+    // }
+    // , decrement(num){
+    //   this.count-=num
+    // },
+    // changeName(event){
+    //   this.name1='batman'
+    //   console.log(event)
+    // }
 
 
 
@@ -206,28 +262,41 @@ export default {
 </script>
 
 <style>
-#app {
+#app{
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.underline {
-  text-decoration: underline;
-}
-
-.promoted {
-  font-style: italic;
-}
-
-.new {
-  color: olivedrab;
 }
 
 .sold-out {
   color: red;
 }
+label {
+  font-weight: bold;
+  display: flex;
+  margin-bottom: 5px;
+}
+input + label {
+  font-weight: bold;
+  display: inline-flex;
+  margin-right: 20px;
+}
+input[type='text'],
+textarea,
+select {
+  display: block;
+  width: 400px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42857143;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
 </style>
+
