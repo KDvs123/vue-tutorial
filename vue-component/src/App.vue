5f21cd7d2fa7078e0b1713554ad531d393c041bb
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-20 16:50:38 +0530

    components in vue

---
+++ vue-component/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <Greet/>
+</template>
+
+<script>
+import Greet from './components/Greet.vue'
+
+
+export default {
+  
+  name: 'App',
+  components: {
+    Greet,
+    
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-component/src/components/Greet.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>Hello Vihanga</h2>
+</template>
+
+<script>
+export default {
+    name:'GreetVue',
+
+}
+</script>
+
+<style>
+
+</style>
