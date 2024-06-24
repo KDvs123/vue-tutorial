47ba89ff2b18f4721ce4aeca0e8f8a49cff50c22
KDvs123 <119438971+KDvs123@users.noreply.github.com>
2024-06-24 14:03:17 +0530

    composition api and replacing data property with ref

---
+++ vue-composition/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <DataVue/>
+</template>
+
+<script>
+
+import DataVue from './components/Data.vue'
+
+
+export default {
+  name: 'App',
+  
+  components: {
+    DataVue
+  
+  },
+
+ 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-composition/src/components/Data.vue
@@ -0,0 +1,33 @@
+<template>
+  <h2>Options -{{ o_firstName }}</h2>
+  <h2>Composition - {{c_firstName}} </h2>
+   <h2>Greet - {{greet}} </h2>
+   <!--for binding to the template we dont need to use .value-->
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  name: "DataVue",
+  setup() {
+    const c_firstName = ref("Clark");
+    c_firstName.value='Diana'
+    const greet=`Hello ${c_firstName.value}`
+
+    //ref is a function returns an reactive an mutalble object
+
+
+    return {
+      c_firstName,
+      greet //when the key is same as value we can use es6 property
+    };
+  },
+  data() {
+    return {
+      o_firstName: "Bruce",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
